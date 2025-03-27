4235ddef9fea4597996206709ba09edef95a70d7
odonnerl <odonnerl@LCB-01>
2025-03-27 12:52:05 -0400

    Added code to print my name

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,7 +9,9 @@ namespace ConsoleApp2
     class Program
     {
         static void Main(string[] args)
-        { }
+        {
+            Console.WriteLine("Ryan O'Donnell");
+        }
            
             public string MyNewProperty { get; set; }
 
